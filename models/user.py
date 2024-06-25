8947984abc8b9eed4614edce617ad338f1e9fecf
Samuella M. Aglago <samuellamanyeaglago@gmail.com>
2024-06-25 19:13:07 +0000

    adding cascade to place relationship

--- models/user.py
+++ models/user.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models import storage_type
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -10,14 +9,8 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
 
-    if storage_type == "db":
-        places = relationship("Place", backref="user")
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    places = relationship("Place", backref="user", cascade="all, delete, delete-orphan")
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
