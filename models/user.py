6b83263716c53c2995296c2acf8b5cfbd9a65b9d
Samuella M. Aglago <samuellamanyeaglago@gmail.com>
2024-06-25 18:57:46 +0000

    condition to check models.storage_type in locals

--- models/user.py
+++ models/user.py
@@ -3,21 +3,22 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models import storage_type
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
 
-    if storage_type == "db":
-        places = relationship("Place", backref="user")
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    if "models.storage_type" in locals():
+        from models import storage_type
+        if storage_type == "db":
+            places = relationship("Place", backref="user")
+            email = Column(String(128), nullable=False)
+            password = Column(String(128), nullable=False)
+            first_name = Column(String(128), nullable=True)
+            last_name = Column(String(128), nullable=True)
+        else:
+            email = ""
+            password = ""
+            first_name = ""
+            last_name = ""
