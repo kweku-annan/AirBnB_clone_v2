ed2aed0a5d58c7de35209cf2a3a34a0adda27f41
Samuella M. Aglago <samuellamanyeaglago@gmail.com>
2024-06-26 18:01:35 +0000

    imported Places for the relationship definition and removed backslash for new line

--- models/user.py
+++ models/user.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
+from models.place import Place
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
@@ -13,5 +13,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationshi\
-        p("Place", backref="user", cascade="all, delete, delete-orphan")
+    places = relationship("Place", backref="user", cascade="all, delete, delete-orphan")
