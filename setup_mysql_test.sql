8d2ea2abb6aacea3b18d0e0e6b05196b523f4694
emma-adu-saah <aduemma07@gmail.com>
2024-08-02 16:24:31 +0000

    Modify setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,7 +7,7 @@ USE hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
 IDENTIFIED BY 'hbnb_test_pwd';
 
-GRANT ALL PRIVILEGES ON *.* TO
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO
 'hbnb_test'@'localhost';
 
 GRANT SELECT ON performance_schema.*
